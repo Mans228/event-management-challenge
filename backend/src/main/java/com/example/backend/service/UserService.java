73ae354099fe2d20d70e95ae96f831fc1f721b1a
Mans228 <zhenismansur@gmail.com>
2025-05-27 17:07:42 +0200

    feat(repo): add repositories for entities

---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.backend.service;
+
+import com.example.backend.model.User;
+import com.example.backend.repository.EventRepository;
+import com.example.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
